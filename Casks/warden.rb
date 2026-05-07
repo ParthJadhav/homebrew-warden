a3c2681357f92746b5f56a8103ba383630b4c976
Parth Jadhav <parth@example.com>
2026-05-07 11:23:33 +0530

    fix: use immutable download URL

--- Casks/warden.rb
+++ Casks/warden.rb
@@ -2,7 +2,7 @@ cask "warden" do
   version "1.2.6"
   sha256 "e0dde08bbbd6963c0d079e04ce5a4aa3dd79fb7ce7fb30e209623ae15854a0ec"
 
-  url "https://download.getwarden.org/Warden.dmg"
+  url "https://download.getwarden.org/Warden-#{version}.dmg"
   name "Warden"
   desc "macOS menu bar screen locking utility"
   homepage "https://www.getwarden.org"
