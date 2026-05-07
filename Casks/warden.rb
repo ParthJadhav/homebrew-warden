40b3398ddbd039594c0e7adc88fa35e5fb70fff7
Parth Jadhav <jadhavparth99@gmail.com>
2026-05-07 13:59:45 +0530

    Use latest Warden cask artifact

--- Casks/warden.rb
+++ Casks/warden.rb
@@ -1,8 +1,8 @@
 cask "warden" do
-  version "2.0.0"
-  sha256 "0d4da9a381d1260d363656c46c9db9bfdecdb7557f00c6742c37babaeef8e849"
+  version :latest
+  sha256 :no_check
 
-  url "https://download.getwarden.org/Warden-#{version}.dmg"
+  url "https://download.getwarden.org/Warden-latest.dmg"
   name "Warden"
   desc "macOS menu bar screen locking utility"
   homepage "https://www.getwarden.org"
